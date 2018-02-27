96b22ecb64eba5df1e3168d90aded4a20751b20b
Will <wimoore2018@mirmanmustangs.org>
2018-02-27 15:04:30 -0800

    Canvas works now!

--- public/draw.js
+++ public/draw.js
@@ -1,31 +1,35 @@
-var bg;
 var drawStarted = false;
 
 function setup(){
     var canvasWidth = windowWidth * 0.7 >= 640 ? 640 : windowWidth * .7;
-    var canvasHeight = windowHeight * 0.7 >= 480 ? 480 : windowWidth * .7;
-
+    var canvasHeight = windowHeight * 0.7 >= 480 ? 480 : windowHeight * .7;
     var c = createCanvas(canvasWidth, canvasHeight);
     background("#181C49");
     document.querySelector("#share").addEventListener("click", share);
-}
-c.mousePressed = function draw() {
-  stroke("white");
-  fill("white");
-  if(mouseIsPressed) {
-    if (!drawStarted) {
-      drawStarted = true;
+    textSize(20);
+    fill("white");
+    text("What do you think the ice caps will look like in 50 years? Draw it!", 10, 30);
+    c.touchStarted(function() {
+      stroke("white");
+      fill("white");
       beginShape();
-      vertex(mouseX,mouseY);
-      //ellipse(mouseX,mouseY,10,10);
-      point(mouseX,mouseY);
-    }
-  }
-}
+      console.log("Shape begun");
+   });
 
-function mouseReleased() {
-  endShape();
-  //drawStarted = false;
+   c.touchMoved(function(){
+    vertex(mouseX,mouseY);
+    point(mouseX,mouseY);
+   }); 
+
+   c.touchEnded(function(){
+     console.log("Mouse released");
+     vertex(mouseX,mouseY);
+      point(mouseX,mouseY);
+     endShape();
+     drawStarted = false;
+   });
+
+   console.log(c);
 }
 
 function share() {
