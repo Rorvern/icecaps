510466a75c23f743b981eec37e4b5fbb6f9a7f64
mirman_mtaggart <mtaggart@mirman.org>
2018-05-03 18:55:22 -0700

    Send data to backend

--- public/draw.js
+++ public/draw.js
@@ -3,6 +3,33 @@ var canvasWidth = window.innerWidth * 0.7 >= 640 ? 640 : window.innerWidth * .7;
 var canvasHeight = window.innerHeight * 0.7 >= 480 ? 480 : window.innerHeight * .7;
 var c;
 
+document.getElementById("share-form")
+.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const backendUrl = "http://apps.mirman.org:3013";
+  const email = document.getElementById("email").value;
+  const image = c.canvas.toDataURL("image/png").replace(/data:image\/png;base64,/, '');
+  const data = {
+    email: email,
+    image: image
+  };
+  const req = new Request(backendUrl,{
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: new Headers({
+      "Content-Type": "application/json"
+    }),
+  });
+
+  console.log(req);
+
+  fetch(req)
+    .then((res) => {
+      console.log(res)
+    })
+    .catch(err => console.error(err));
+});
+
 function setup(){
     initializeCanvas();
     document.querySelector("#share").addEventListener("click", share);
@@ -26,7 +53,6 @@ function setup(){
      drawStarted = false;
    });
 
-   console.log(c);
 }
 
 function share() {
